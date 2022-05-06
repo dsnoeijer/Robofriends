4f281910f979373791c61fef71af7092e29b8fa6
WD Snoeijer <88512452+dsnoeijer@users.noreply.github.com>
2022-05-06 19:01:39 +0200

    implemented redux-logger fix

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import 'tachyons';
 import { searchRobots } from './reducers';
 
 
-const logger = createLogger;
+const logger = createLogger();
 const store = createStore(searchRobots, applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
